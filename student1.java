76df9f7f0094a9f2db06b746d6b144968f8118f8
Pablo Carmona <67069515+DjN3lson@users.noreply.github.com>
2023-01-24 10:14:18 -0500

    Add files via upload

---
+++ student1.java
@@ -0,0 +1,32 @@
+package student1;
+public class student1 {
+    private String studentId;
+    private String studentName;
+
+    //https://github.com/DjN3lson/student1.git
+    // token: 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
+
+
+    public student1(String studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public String getStudentId() {
+        return this.studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return this.studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    
+}
