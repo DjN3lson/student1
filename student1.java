2ef74d47965e4c96a6a87f8727347519529626ac
Pablo Carmona <67069515+DjN3lson@users.noreply.github.com>
2023-01-24 16:55:24 -0500

    git commit -a

--- student1.java
+++ student1.java
@@ -2,10 +2,20 @@ package student1;
 public class student1 {
     private String studentId;
     private String studentName;
+    private String address;
 
-    public student1(String studentId, String studentName) {
+    public student1(String studentId, String studentName, address) {
         this.studentId = studentId;
         this.studentName = studentName;
+        this.address = address;
+    }
+
+    public String getAddress(){
+        return this. address;
+    }
+
+    public void getAddress(String address){
+        this.address = address;
     }
 
     public String getStudentId() {
