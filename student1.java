e3195151c9f908d76541438d4e10f176bb12fa8c
Pablo Carmona <67069515+DjN3lson@users.noreply.github.com>
2023-01-24 10:34:34 -0500

    Update student1.java

--- student1.java
+++ student1.java
@@ -3,10 +3,6 @@ public class student1 {
     private String studentId;
     private String studentName;
 
-    //https://github.com/DjN3lson/student1.git
-    // token: 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
-
-
     public student1(String studentId, String studentName) {
         this.studentId = studentId;
         this.studentName = studentName;
